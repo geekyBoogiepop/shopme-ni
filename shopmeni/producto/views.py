198bdb92407b3f0252e506ca2d5cf3681f6a559f
geekyBoogiepop <fljustsocial@outlook.com>
2023-11-04 19:55:02 -0600

    feat: visualizar productos relacionados. fix: corrección  en la altura de las imagenes en los detalles del producto

--- shopmeni/producto/views.py
+++ shopmeni/producto/views.py
@@ -4,9 +4,11 @@ from .models import Producto
 
 def detalles(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
+    productos_relacionados = Producto.objects.filter(categoria=producto.categoria, is_sold=False).exclude(pk=pk)[:3]
 
     context = {
-        "producto": producto
+        "producto": producto,
+        "productos_relacionados": productos_relacionados
     }
 
     return render(request, "producto/detalles.html", context)
