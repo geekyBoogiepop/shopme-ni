c3696c8444634a90875237934116ee10c1ff9f25
geekyBoogiepop <fljustsocial@outlook.com>
2023-11-04 18:31:45 -0600

    feat: visualizar detalles del producto y vendedor

--- shopmeni/producto/views.py
+++ shopmeni/producto/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Producto
+
+def detalles(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+
+    context = {
+        "producto": producto
+    }
+
+    return render(request, "producto/detalles.html", context)
