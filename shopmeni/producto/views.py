69fc7b843fc67e28bd6423a107e930eb6788cd8e
geekyBoogiepop <fljustsocial@outlook.com>
2023-11-12 22:18:24 -0600

    fix: corregir formulario para editar un producto

--- shopmeni/producto/views.py
+++ shopmeni/producto/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import FormularioNuevoProducto
+from .forms import FormularioNuevoProducto, FormularioEditarProducto
 from .models import Producto, Categoria
 
 def productos(request):
@@ -64,14 +64,14 @@ def editar(request, pk):
     producto = get_object_or_404(Producto, pk=pk, created_by=request.user)
 
     if request.method == "POST":
-        form = FormularioNuevoProducto(request.POST, request.FILES, instance=producto)
+        form = FormularioEditarProducto(request.POST, request.FILES, instance=producto)
 
         if form.is_valid():
             producto.save()
 
             return redirect("producto:detalles", pk=producto.id) # type: ignore
         
-    form = FormularioNuevoProducto(instance=producto)
+    form = FormularioEditarProducto(instance=producto)
     context = {
         "form": form,
         "titulo": "Editar producto"
